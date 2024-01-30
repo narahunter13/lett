489b46611499c6b87dea717104c684508cbaf5e1
narahunter13 <narahunter13@gmail.com>
2024-01-30 16:14:12 +0700

    Dashboard UI

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -73,7 +73,7 @@
             <div class="content">
                 <div class="container-fluid">
                     <div class="row">
-                        
+                        {{ $slot }}
                     </div>
                 </div>
             </div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,4 +2,28 @@
     <x-slot:title>
         Dashboard
     </x-slot:title>
+    <div class="col-lg-3 col-12">
+        <div class="small-box bg-success">
+            <div class="inner">
+                <h3>100</h3>
+
+                <p>Surat Masuk</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-inbox"></i>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-12">
+        <div class="small-box bg-danger">
+            <div class="inner">
+                <h3>100</h3>
+
+                <p>Surat Keluar</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-envelope-open"></i>
+            </div>
+        </div>
+    </div>
 </x-layout>
